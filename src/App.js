3f6cfe68d3eedfc7d0966929a5fc9571f24b8b55
Bibhushan <bibhus47@in.ibm.com>
2024-05-10 14:17:59 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState(null);
+
+  const handleFirstName = (e)=>{
+    setFirstName(e.target.value);
+  }
+
+  const handleLastName = (e)=>{
+    setLastName(e.target.value);
+  }
+
+  const handleFullName = (e)=>{
+    e.preventDefault();
+    let fullName = null;
+
+    if (firstName === ''){
+      console.log('first name is null');
+      setFullName(null);
+    } else if (lastName === ''){
+      console.log('last name is null');
+      setFullName(null);
+    } else {
+      setFullName(firstName + " " + lastName);
+    }
+
+    return fullName;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <form>
+        <h1>Full Name Display</h1>
+        <p><span>First Name: </span><input type='text' onChange={handleFirstName}/></p>
+        <p><span>Last Name: </span><input type='text' onChange={handleLastName}/></p>
+        <button onClick={handleFullName}>Submit</button>
+        {fullName && <p>{fullName}</p>}
+      </form>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +55,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
